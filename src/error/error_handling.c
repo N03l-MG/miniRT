7e33e3b61e503ed939daa28ba605c97e9b8ff872
Noel Monzon <nmonzon@2-D-2.42heilbronn.de>
2025-03-13 16:20:43 +0100

    gc and shape files

--- src/error/error_handling.c
+++ src/error/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: nmonzon <nmonzon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 18:26:09 by nmonzon           #+#    #+#             */
-/*   Updated: 2025/03/12 15:23:23 by nmonzon          ###   ########.fr       */
+/*   Updated: 2025/03/13 16:14:44 by nmonzon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static char	*get_error_message(t_error error)
 {
 	static char	*messages[] = {
-	[ERR_MEMORY] = RED "Fatal memory error!" RESET,
+	[ERR_MEMORY] = RED "Memory allocation error." RESET,
 	[ERR_ARGS] = RED "Invalid arguments. Usage: ./miniRT [.rt file/path]" RESET,
 	[ERR_WINDOW] = RED "Failed to create MLX window." RESET,
 	[ERR_IMAGE] = RED "Failed to create MLX image." RESET,
@@ -25,12 +25,21 @@ static char	*get_error_message(t_error error)
 	return (messages[error]);
 }
 
-void	handle_error(t_error error, t_window_data *window_data)
+void	error(t_error error, t_window_data *window_data)
 {
 	char	*message;
 
 	message = get_error_message(error);
 	ft_fprintf(2, "%s\n", message);
+}
+
+void	error_fatal(t_error error, t_window_data *window_data)
+{
+	char	*message;
+
+	message = get_error_message(error);
+	ft_fprintf(2, "Fatal: %s\n", message);
+	ft_fprintf(2, "Exiting miniRT.\n");
 	clean_exit(EXIT_FAILURE, window_data);
 }
 
@@ -43,6 +52,6 @@ void	clean_exit(int status, t_window_data *window_data)
 		if (window_data->mlx_window)
 			mlx_terminate(window_data->mlx_window);
 	}
-	//gc_collect();
+	gc_collect();
 	exit(status);
 }
